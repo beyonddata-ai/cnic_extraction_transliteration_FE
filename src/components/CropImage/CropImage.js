3ef462d94ba93feea3158171cc4f20a0fcf2e519
zabeeh ullah khan <zullah.bese15seecs@seecs.edu.pk>
2022-04-14 15:52:30 +0500

    translation changes

--- src/components/CropImage/CropImage.js
+++ src/components/CropImage/CropImage.js
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactCrop from "react-image-crop";
-import "react-image-crop/dist/ReactCrop.css";
+// import React from "react";
+// import ReactCrop from "react-image-crop";
+// import "react-image-crop/dist/ReactCrop.css";
 
-const CropImage = ({ chimg, setchimg }) => {
-  return (
-    <>
-      {chimg && (
-        <div>
-          <ReactCrop
-            style={{ maxWidth: "50%" }}
-            src={chimg}
-            onImageLoaded={setchimg}
-            crop={{ aspect: 16 / 9 }}
-            // onChange={setCrop}
-          />
-          <Button>crop</Button>
-        </div>
-      )}
-    </>
-  );
-};
+// const CropImage = ({ chimg, setchimg }) => {
+//   return (
+//     <>
+//       {chimg && (
+//         <div>
+//           <ReactCrop
+//             style={{ maxWidth: "50%" }}
+//             src={chimg}
+//             onImageLoaded={setchimg}
+//             crop={{ aspect: 16 / 9 }}
+//             // onChange={setCrop}
+//           />
+//           <Button>crop</Button>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
 
-export default CropImage;
+// export default CropImage;
